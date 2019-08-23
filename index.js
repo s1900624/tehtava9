6e3d6caef5e76c14cf1933addf001b2c29e732dc
Ilmast Toivo <s1900624@edu.bc.fi>
2019-08-23 14:14:28 +0300

    Add: view ´arr1´ and ´arr2´ arrays with keys and values

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 window.onload = function() {
+    const obj = {
+        coucou: "hello",
+        comment: "how",
+        va: "are",
+        tu: "you",
+    };
 
+    const arr1 = [];
+    const arr2 = [];
+
+    Object.keys(obj).map(function(key) {
+        arr1.push(key);
+        arr2.push(obj[key]);
+        return true;
+    });
+
+    console.log(arr1, arr2);
 };
